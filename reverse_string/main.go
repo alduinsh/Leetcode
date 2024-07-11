daa3cadffc640b66607a4dbd5a1a6d4a4c4725b4
Liudmila Mihaylichenko <lucy.mihaylichenko@gmail.com>
2024-07-11 17:21:10 +0300

    Add var2 for reverse_task

--- reverse_string/main.go
+++ reverse_string/main.go
@@ -2,6 +2,8 @@
 //Write a function that reverses a string.
 //The input string is given as an array of characters s.
 
+//var 1
+/*
 package main
 
 import "fmt"
@@ -14,6 +16,28 @@ func main() {
 }
 
 func reverseString(s []byte) {
+	left, right := 0, len(s)-1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
+*/
+
+// var2 for unicode and >1byte
+package main
+
+import "fmt"
+
+var name = []rune("Людмила")
+
+func main() {
+	reverseString(name)
+	fmt.Println(string(name))
+}
+
+func reverseString(s []rune) {
 	left, right := 0, len(s)-1
 	for left < right {
 		s[left], s[right] = s[right], s[left]
