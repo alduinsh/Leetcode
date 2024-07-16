9dc95aa2959f338605f6ef043c4d74e362ad111f
Liudmila Mihaylichenko <lucy.mihaylichenko@gmail.com>
2024-07-16 11:53:07 +0300

    Add new var2 for single_number

--- single_number/main.go
+++ single_number/main.go
@@ -1,7 +1,14 @@
+/*
+136. Single Number
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
+
 package main
 
 import "fmt"
 
+/*var 1
 func main() {
 	result := singleNumber([]int{2, 2, 1, 4, 5, 4, 5})
 	fmt.Println("Single number:", result)
@@ -13,4 +20,27 @@ func singleNumber(nums []int) int {
 		result ^= num
 	}
 	return result
+}
+*/
+
+//var 2
+
+func main() {
+	//инициализируем срез целых чисел
+	nums := []int{2, 3, 3, 4, 4, 10, 5, 5}
+	result := singleNumber(nums)
+	fmt.Println(result)
+}
+
+func singleNumber(nums []int) int {
+	uniq := make(map[int]int)
+	for _, n := range nums {
+		uniq[n]++
+	}
+	for num, count := range uniq {
+		if count == 1 {
+			return num
+		}
+	}
+	return 0
 }
