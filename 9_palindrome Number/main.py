d2b0b9abaa78caf7003f360d16770be83719ab7b
Liudmila Mihaylichenko <lucy.mihaylichenko@gmail.com>
2025-03-22 15:29:25 +0300

    new 9 task

---
+++ 9_palindrome Number/main.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False  # Отрицательные числа не могут быть палиндромами
+        
+        reverse = 0
+        xcopy = x
+        
+        while x > 0:
+            reverse = (reverse * 10) + (x % 10)
+            x //= 10
+        
+        return reverse == xcopy
+
+if __name__ == "__main__":
+    solution = Solution()
+    user_input = input("Введите числа (через пробел): ")
+    numbers = user_input.split()
+    
+    for num in numbers:
+        try:
+            num_int = int(num)
+            if solution.isPalindrome(num_int):
+                print(f"{num_int} является палиндромом")
+            else:
+                print(f"{num_int} не является палиндромом")
+        except ValueError:
+            print(f"{num} не является целым числом")
+    
